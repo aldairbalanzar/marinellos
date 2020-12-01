908dcf7a0db2f197d753e95b88b2f98799e857f6
aldairbalanzar <abalanzar7@gmail.com>
2020-11-30 23:53:31 -0500

    setup styling for Gallery component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,7 +3,10 @@ import moustache from '../assets/moustache.jpg';
 
 const Gallery = () => {
     return (
-        <section className='gallery-section'>
+        <section className='gallery-section' id='gallery'>
+            <header className='gallery-header'>
+                <h1 className='gallery-title'>GALER&Iacute;A</h1>
+            </header>
             <div className='image-container'>
                 <img src={moustache} alt="" className="gallery-img"/>
                 <img src={moustache} alt="" className="gallery-img"/>
