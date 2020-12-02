441b0effc030ca30f9956300b0197d0bb8ae2103
aldairbalanzar <abalanzar7@gmail.com>
2020-12-02 14:19:42 -0500

    started Footer component and added instagram elemnt to Gallery

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="social-links"></div>
+            <div className="footer-nav"></div>
+        </footer>
+    )
+}
+
+export default Footer
