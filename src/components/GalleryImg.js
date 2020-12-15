75ff014315bee9fdf77402aad6bb7aa87f29681c
aldairbalanzar <abalanzar7@gmail.com>
2020-12-15 11:38:03 -0500

    debugging instagram feed

--- src/components/GalleryImg.js
+++ src/components/GalleryImg.js
@@ -2,12 +2,13 @@ import React from 'react'
 import wavy_hair from '../assets/wavy_hair.jpg'
 
 const GalleryImg = ({ post }) => {
+    console.log('post: ', post)
     return (
         <>
             <img 
             className='gallery-img'
-            src={post.thumbnail_src || wavy_hair}
-            alt={post.accessibility_caption || 'placeholder'}
+            src={post ? post.node.thumbnail_src : wavy_hair}
+            alt={post ? post.node.accessibility_caption : 'placeholder'}
             />
         </>
     )
