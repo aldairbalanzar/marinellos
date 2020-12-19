b1dce7526b035f82f84c77a9ca460c19bb128011
aldairbalanzar <abalanzar7@gmail.com>
2020-12-18 23:53:03 -0500

    added content to Footer component and edited styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer>
-            <div className="social-links"></div>
-            <div className="footer-nav"></div>
+        <footer className='footer-section'>
+            <p className="copyright-statement">
+                    &#169; Marinellos Beauty Salon 2020 | All Rights Reserved.
+            </p>
+            <div className="footer-social-links">
+                <button className='footer-social-media-btn'>
+                    <i className='footer-social-icon fab fa-facebook-square social-media-icon'></i>
+                </button>
+                <button className='footer-social-media-btn'>
+                    <a href="https://www.instagram.com/marinellosbeautysalon/">
+                        <i className='footer-social-icon fab fa-instagram-square social-media-icon'></i>
+                    </a>
+                </button>
+            </div>
         </footer>
     )
 }
