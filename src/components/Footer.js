f911704f28b8c9cb76d6a93bd7aa6243a2661439
aldairbalanzar <abalanzar7@gmail.com>
2020-12-22 17:35:24 -0500

    added social links header to footer and styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
                     &#169; Marinellos Beauty Salon 2020 | All Rights Reserved.
             </p>
             <div className="footer-social-links">
+                <h3 className="social-footer-header">Redes Sociales</h3>
                 <button className='footer-social-media-btn'>
                     <i className='footer-social-icon fab fa-facebook-square social-media-icon'></i>
                 </button>
